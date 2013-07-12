2f91733cdb8e4beed93b8c19561fae2ce61c2638
Dan <Dan@Dinkydetails.com>
2013-07-11 22:23:13 -0400

    Commiting code thus far working on Web API ASYNC

--- TopTrackApp/src/com/dannis/toptrackapp/LastFMWebAPITask.java
+++ TopTrackApp/src/com/dannis/toptrackapp/LastFMWebAPITask.java
@@ -1,5 +1,24 @@
 package com.dannis.toptrackapp;
 
-public class LastFMWebAPITask {
 
+import java.util.ArrayList;
+import android.os.AsyncTask;
+
+
+public class LastFMWebAPITask extends AsyncTask<String, Integer, String>
+{
+	private ProgressDialog progDialog;
+	private Context context; 
+	private TopTrackListActivity activity;
+	private static final String debugTag = "LastFMWebAPITask";
+	
+	public LastFMWebAPITask(TopTrackListActivity activity){
+		super();
+		this.activity = activity;
+		this.context = this.activity.getApplicationContext();
+	}
+	
+	@Override
+	protected void onPreExecute();
+	
 }
