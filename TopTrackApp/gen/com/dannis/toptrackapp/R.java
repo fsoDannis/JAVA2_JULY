b15e77b14fb45c53add940b8f2fe8de251131544
Dan <Dan@Dinkydetails.com>
2013-07-12 00:09:05 -0400

    Changed some coding and added images/colors/strings

--- TopTrackApp/gen/com/dannis/toptrackapp/R.java
+++ TopTrackApp/gen/com/dannis/toptrackapp/R.java
@@ -8,25 +8,66 @@
 package com.dannis.toptrackapp;
 
 public final class R {
+    public static final class array {
+        public static final int metros=0x7f060000;
+    }
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040002;
+        public static final int contents_text=0x7f040003;
+        public static final int encode_view=0x7f040004;
+        public static final int gray=0x7f040000;
+        public static final int help_button_view=0x7f040005;
+        public static final int help_view=0x7f040006;
+        public static final int result_image_border=0x7f040007;
+        public static final int result_minor_text=0x7f040008;
+        public static final int result_points=0x7f040009;
+        public static final int result_text=0x7f04000a;
+        public static final int result_view=0x7f04000b;
+        public static final int sbc_header_text=0x7f04000c;
+        public static final int sbc_header_view=0x7f04000d;
+        public static final int sbc_layout_view=0x7f04000f;
+        public static final int sbc_list_item=0x7f04000e;
+        public static final int sbc_page_number_text=0x7f040010;
+        public static final int sbc_snippet_text=0x7f040011;
+        public static final int share_text=0x7f040012;
+        public static final int share_view=0x7f040013;
+        public static final int status_text=0x7f040015;
+        public static final int status_view=0x7f040014;
+        public static final int transparent=0x7f040016;
+        public static final int viewfinder_frame=0x7f040017;
+        public static final int viewfinder_laser=0x7f040018;
+        public static final int viewfinder_mask=0x7f040019;
+        public static final int white=0x7f040001;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int blank=0x7f020000;
+        public static final int filler_icon=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int rowbackground=0x7f020003;
+        public static final int rowbackgroundselected=0x7f020004;
+        public static final int rowselector=0x7f020005;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int RelativeLayout01=0x7f080000;
+        public static final int album_icon=0x7f080005;
+        public static final int artist_name=0x7f080007;
+        public static final int background=0x7f080004;
+        public static final int metro=0x7f080001;
+        public static final int track_button=0x7f080002;
+        public static final int track_list=0x7f080003;
+        public static final int track_name=0x7f080006;
     }
     public static final class layout {
-        public static final int activity_top_track_list=0x7f030000;
-        public static final int main=0x7f030001;
-    }
-    public static final class menu {
-        public static final int activity_top_track_list=0x7f060000;
+        public static final int main=0x7f030000;
+        public static final int trackrow=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int looking_for_tracks=0x7f050001;
+        public static final int no_tracks=0x7f050002;
+        public static final int track_button_label=0x7f050003;
     }
     public static final class style {
         /** 
@@ -50,10 +91,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f070001;
     }
 }
