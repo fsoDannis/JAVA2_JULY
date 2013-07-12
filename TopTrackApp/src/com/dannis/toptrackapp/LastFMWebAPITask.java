36ba44f715e90c9d89a0cc5c89fbafc420452185
Dan <Dan@Dinkydetails.com>
2013-07-11 22:38:42 -0400

    Just adding more code for the fetch/parse

--- TopTrackApp/src/com/dannis/toptrackapp/LastFMWebAPITask.java
+++ TopTrackApp/src/com/dannis/toptrackapp/LastFMWebAPITask.java
@@ -2,6 +2,11 @@ package com.dannis.toptrackapp;
 
 
 import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 
 
@@ -19,6 +24,43 @@ public class LastFMWebAPITask extends AsyncTask<String, Integer, String>
 	}
 	
 	@Override
-	protected void onPreExecute();
+	protected void onPreExecute(){
+	super.onPreExecute();
+	progDialog = ProgressDialog.show(this.activity, "Search", this.context.gatResources().getString(R.string.looking_for_tracks), true,false);
+	}
 	
+	@Override
+	protected String doInBackground(String... params){
+		try{
+			log.d(debugTag,"Background:"+ Thread.currentThread().getName());
+			String result = LastFMHelper.downloadFromServer(params);
+			return result;
+		}catch (Exception e){
+			return new String();
+		}
+	}
+
+	@Override
+	protected void onPostExecute(String result)
+	{
+		ArrrayList<TrackData> trackdata = new ArrayList,TrackData>();
+		
+		progDialog.dismiss();
+		if(result.length()==0){
+			this.activity.alert("Unable to find track data. Try again later.");
+			return;
+		}
+		
+		try{
+			JSONObject respObj = new JSONObject(result);
+			JSONObject topTracksObj = respObj.getJSONObject("toptracks");
+			JSONArray tracks = topTracksObj.getJSONArray("track");
+			for(int i=0; i,tracks.length(); i++){
+				JSONObject track = tracks.getJSONObject(i);
+				String trackName = tracks.getString("name");
+				String trackUrl = tracks.getString("url");
+			}
+			
+		}
+	}
 }
