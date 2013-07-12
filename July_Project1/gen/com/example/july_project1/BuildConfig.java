0fd8458899a8472935699b4ca5ddab2e024a415f
Dan <Dan@Dinkydetails.com>
2013-07-11 22:12:33 -0400

    Ran a Clean

--- July_Project1/gen/com/example/july_project1/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.july_project1;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- TopTrackApp/gen/com/dannis/toptrackapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.dannis.toptrackapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
